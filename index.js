be4f44beb11932dc246d1e2a4b2a699e65c8021e
mahfuja5768 <mahfujaahsanmuna@gmail.com>
2023-10-19 23:16:16 +0600

    added single-product route

--- index.js
+++ index.js
@@ -9,7 +9,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_PASSWORD}@cluster0.mz3fw7v.mongodb.net/?retryWrites=true&w=majority`;
 console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -21,30 +20,66 @@ const client = new MongoClient(uri, {
   },
 });
 
-
 async function run() {
-    try {
-      // Connect the client to the server	(optional starting in v4.7)
-      await client.connect();
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const brandCollection = client
+      .db("Vogue-Verse-Apparel-store")
+      .collection("brands");
+
+    const brandProductCollection = client
+      .db("Vogue-Verse-Apparel-store")
+      .collection("brandsProduct");
+
+    const myCartCollection = client
+      .db("Vogue-Verse-Apparel-store")
+      .collection("myCart");
+
+    app.get("/brands", async (req, res) => {
+      const result = await brandCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/brandsProduct", async (req, res) => {
+      const result = await brandProductCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/brandsProduct/:brandName", async (req, res) => {
+      const name = req.params.brandName;
+      const query = { brandName: name };
+      const result = await brandProductCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.get("/brandsProduct", async (req, res) => {
+      const result = await brandProductCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/single-product/:id", async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const query = { _id: new ObjectId(id) };
+      const result = await brandProductCollection.find(query).toArray();
+      res.send(result);
+    });
+
   
-      const brandCollection = client.db("Vogue-Verse-Apparel-store").collection("brands");
-  
-      app.get("/brands", async (req, res) => {
-        const result = await brandCollection.find().toArray();
-        res.send(result);
-      });
 
 
-    } finally {
-      // await client.close();
-    }
+  } finally {
+    // await client.close();
   }
-  run().catch(console.log);
-  
-  app.get("/", (req, res) => {
-    res.send("Coffee making server is running....");
-  });
-  
-  app.listen(port, () => {
-    console.log(`Coffee making Server is running on ${port}`);
-  });
+}
+run().catch(console.log);
+
+app.get("/", (req, res) => {
+  res.send("server is running....");
+});
+
+app.listen(port, () => {
+  console.log(` Server is running on ${port}`);
+});
